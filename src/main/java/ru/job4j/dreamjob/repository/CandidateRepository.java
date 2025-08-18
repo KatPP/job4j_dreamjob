be9a1e7bc482aa8d80cd0c3dbb8206abb269ca11
zenit1925 <kostia-85-85@yandex.ru>
2025-08-18 14:38:10 +0300

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/repository/CandidateRepository.java
@@ -0,0 +1,18 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Candidate;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CandidateRepository {
+
+    void saveCandidate(Candidate candidate);
+
+    void deleteByIdCandidate(int id);
+
+    boolean updateCandidate(Candidate candidate);
+
+    Optional<Candidate> findByIdCandidate(int id);
+
+    Collection<Candidate> findAllCandidate();
+}
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -0,0 +1,66 @@
+package ru.job4j.dreamjob.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class MemoryCandidateRepository implements CandidateRepository {
+
+    private static final MemoryCandidateRepository INSTANCE_NEW = new MemoryCandidateRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Candidate> candidaties = new HashMap<>();
+
+    private MemoryCandidateRepository() {
+        saveCandidate(new Candidate(0, "Bob", "просто описание", LocalDateTime.now()));
+        saveCandidate(new Candidate(0, "Cat", "просто описание", LocalDateTime.now()));
+        saveCandidate(new Candidate(0, "Robert", "просто описание", LocalDateTime.now()));
+        saveCandidate(new Candidate(0, "Pavel", "просто описание", LocalDateTime.now()));
+        saveCandidate(new Candidate(0, "Max", "просто описание", LocalDateTime.now()));
+        saveCandidate(new Candidate(0, "Fred", "просто описание", LocalDateTime.now()));
+    }
+
+    public static MemoryCandidateRepository getInstance() {
+        return INSTANCE_NEW;
+    }
+
+    @Override
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(nextId++);
+        candidate.setCreationDate(LocalDateTime.now());
+        candidaties.put(candidate.getId(), candidate);
+    }
+
+    @Override
+    public void deleteByIdCandidate(int id) {
+        candidaties.remove(id);
+    }
+
+    @Override
+    public boolean updateCandidate(Candidate candidate) {
+        return candidaties.computeIfPresent(candidate.getId(),
+                (id, oldCandidate) -> new Candidate(
+                        oldCandidate.getId(),
+                        candidate.getName(),
+                        candidate.getDescription(),
+                        oldCandidate.getCreationDate()
+                )) != null;
+    }
+
+    @Override
+    public Optional<Candidate> findByIdCandidate(int id) {
+        return Optional.ofNullable(candidaties.get(id));
+    }
+
+    @Override
+    public Collection<Candidate> findAllCandidate() {
+        return candidaties.values();
+    }
+}
